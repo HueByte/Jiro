1eea5bbcb7e73e553d5f8777520f5d02225e83c7
huebyte <ihuebyte@gmail.com>
2022-12-19 18:24:37 +0100

    Applied models & Added new defaults to client config

---
+++ src/Jiro.Communication/graphs.py
@@ -0,0 +1,3 @@
+import plotext as plt
+
+print("bonk")
--- src/Jiro.Communication/jiro.py
+++ src/Jiro.Communication/jiro.py
@@ -4,51 +4,28 @@ import sharedStorage
 import asyncio
 import json
 import lib
+from models import jiro_models
 
 
-async def send_request(prompt):
-    body = JiroRequest(prompt)
+async def send_request(prompt: str) -> jiro_models.JiroResponse:
+    body = jiro_models.JiroRequest(prompt)
     requestUrl = sharedStorage.config['jiroUrl']
 
     response = requests.post(
         f'{requestUrl}/api/jiro', json={"prompt": body.prompt})
 
-    return JiroResponse(response.content)
+    return jiro_models.JiroResponse(response.text)
 
 
-async def print_response_message(message):
+async def print_response_message(message: str) -> None:
     sys.stdout.write(f"\n{lib.colors.JIRO}[Jiro]$ ")
     sys.stdout.flush()
+
+    speed = sharedStorage.config['chatSpeed']
 
     for char in message:
         sys.stdout.write(char)
         sys.stdout.flush()
-        await asyncio.sleep(0.02)
+        await asyncio.sleep(speed)
 
     print(f'{lib.colors.ENDC}\n')
-
-
-class JiroRequest:
-    def __init__(self, data):
-        self.prompt = data
-
-
-class JiroResponse:
-    def __init__(self, result, isSuccess, errors):
-        self.result = result
-        self.isSuccess = isSuccess
-        self.errors = errors
-        self.type = "temp"
-
-    def __init__(self, jsonString):
-        self.__dict__ = json.loads(jsonString)
-
-
-class JiroChat:
-    def __init__(self) -> None:
-        pass
-
-
-class Weather:
-    def __init__(self) -> None:
-        pass
