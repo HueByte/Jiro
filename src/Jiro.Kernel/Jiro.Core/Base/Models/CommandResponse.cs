e8d084bc44274f2f55ef94e05a7c7c87a4311a87
huebyte <ihuebyte@gmail.com>
2022-12-19 19:29:44 +0100

    Return executed command name when called

--- src/Jiro.Kernel/Jiro.Core/Base/Models/CommandResponse.cs
+++ src/Jiro.Kernel/Jiro.Core/Base/Models/CommandResponse.cs
@@ -3,6 +3,7 @@ namespace Jiro.Core.Base;
 public class CommandResponse
 {
     public ICommandResult? Result { get; set; }
+    public string? CommandName { get; set; }
     public bool IsSuccess { get; set; }
     public List<string> Errors { get; set; } = new();
 }
