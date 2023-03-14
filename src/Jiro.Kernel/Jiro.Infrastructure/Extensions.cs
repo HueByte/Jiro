47c7c200b19691f1e3c61ce5259bae281159edee
huebyte <ihuebyte@gmail.com>
2023-03-14 13:47:27 +0100

    Created infrastructure project & db context

---
+++ src/Jiro.Kernel/Jiro.Infrastructure/Extensions.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jiro.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddJiroMySQLContext(this IServiceCollection services, string conn)
+        {
+            services.AddDbContext<JiroContext>(options =>
+            {
+                options.UseMySql(ServerVersion.AutoDetect(conn),
+                    x => x.MigrationsAssembly(typeof(JiroContext).Assembly.GetName().Name));
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddJiroSQLiteContext(this IServiceCollection services, string conn)
+        {
+            services.AddDbContext<JiroContext>(options =>
+            {
+                options.UseSqlite($"Data Source={conn}",
+                    x => x.MigrationsAssembly(typeof(JiroContext).Assembly.GetName().Name));
+            });
+
+            return services;
+        }
+    }
+}
