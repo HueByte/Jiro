3096dda78704ac7bda98ae64c6f4bee64843f520
huebyte <ihuebyte@gmail.com>
2023-03-23 12:16:03 +0100

    Cleaning

--- src/Jiro.Kernel/Jiro.Api/clientapp/src/pages/homepage/components/MorphContainer.jsx
+++ src/Jiro.Kernel/Jiro.Api/clientapp/src/pages/homepage/components/MorphContainer.jsx
@@ -1,36 +1,6 @@
-import { useEffect } from "react";
 import jiroAvatar from "../../assets/Jiro.png";
-import anime from "animejs/lib/anime.es.js";
 
 const MorphAvatar = () => {
-  useEffect(() => {
-    // let source = document
-    //   .getElementById("path1")
-    //   .attributes.getNamedItem("d").value;
-
-    // let target = document
-    //   .getElementById("path2")
-    //   .attributes.getNamedItem("d").value;
-    // console.log(source, target);
-
-    anime({
-      targets: "#morphing .p",
-      d: [
-        {
-          value:
-            "M171.5,-191.9C215.3,-167.6,239.1,-107,234.1,-53.5C229,0.1,195,46.5,157.5,71.5C119.9,96.5,78.8,99.9,37.7,123.4C-3.3,146.9,-44.2,190.4,-72.4,185.9C-100.7,181.3,-116.1,128.7,-133.1,84.2C-150.1,39.7,-168.6,3.3,-177.6,-46.2C-186.7,-95.8,-186.2,-158.6,-155,-185.1C-123.8,-211.6,-61.9,-201.8,1,-202.9C63.9,-204.1,127.7,-216.2,171.5,-191.9Z",
-        },
-        {
-          value:
-            "M95,-109.2C143.2,-72.7,216.4,-61.9,232.3,-32C248.2,-2.2,206.8,46.6,170.7,88.7C134.7,130.7,103.9,165.9,67.2,175C30.6,184.2,-12.1,167.3,-52.4,150C-92.8,132.7,-130.8,115.1,-141.6,86.6C-152.3,58,-135.7,18.5,-134,-29.3C-132.4,-77.1,-145.6,-133.1,-125.7,-174.6C-105.8,-216.1,-52.9,-243.1,-14.8,-225.5C23.4,-207.8,46.7,-145.7,95,-109.2Z",
-        },
-      ],
-      easing: "easeInOutSine",
-      duration: 10000,
-      direction: "infinite alternate",
-      loop: true,
-    });
-  });
   return (
     <div
       className={`relative h-fit w-[256px] flex-shrink-0 overflow-visible rounded-full transition duration-1000 lg:mx-auto lg:w-[196px] md:w-[128px]`}
