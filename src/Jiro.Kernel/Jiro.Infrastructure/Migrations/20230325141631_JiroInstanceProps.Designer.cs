03db6d5ccae167c3b59355c9256d19b72808f50a
HueByte <ihuebyte@gmail.com>
2023-03-25 19:50:29 +0100

    DB model with JiroInstance

--- src/Jiro.Kernel/Jiro.Infrastructure/Migrations/20230325141631_JiroInstanceProps.Designer.cs
+++ src/Jiro.Kernel/Jiro.Infrastructure/Migrations/20230325141631_JiroInstanceProps.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Jiro.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Jiro.Infrastructure.Migrations
 {
     [DbContext(typeof(JiroContext))]
-    partial class JiroContextModelSnapshot : ModelSnapshot
+    [Migration("20230325141631_JiroInstanceProps")]
+    partial class JiroInstanceProps
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.2");
