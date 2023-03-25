c96ade33ed37cb49eb72250e7cbfea1431b4e6c7
HueByte <ihuebyte@gmail.com>
2023-03-25 01:37:09 +0100

    Whitelist override ID

--- src/Jiro.Kernel/Jiro.Core/Models/WhitelistEntry.cs
+++ src/Jiro.Kernel/Jiro.Core/Models/WhitelistEntry.cs
@@ -4,7 +4,7 @@ namespace Jiro.Core.Models
 {
     public class WhiteListEntry : DbModel<string>
     {
-        public string Id { get; set; } = default!;
+        public override string Id { get; set; } = default!;
         public DateTime AddedDate { get; set; }
         public string UserId { get; set; } = default!;
         public virtual AppUser User { get; set; } = default!;
