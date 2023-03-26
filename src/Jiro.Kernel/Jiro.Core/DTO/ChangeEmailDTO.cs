96c0ddb3b56906e89711731a9261f74e2a4ddbfe
HueByte <ihuebyte@gmail.com>
2023-03-27 01:06:20 +0200

    Added missing ChangeEmailDTO and ChangePasswordDTO

---
+++ src/Jiro.Kernel/Jiro.Core/DTO/ChangeEmailDTO.cs
@@ -0,0 +1,8 @@
+namespace Jiro.Core.DTO
+{
+    public class ChangeEmailDTO
+    {
+        public string NewEmail { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
---
+++ src/Jiro.Kernel/Jiro.Core/DTO/ChangePasswordDTO.cs
@@ -0,0 +1,8 @@
+namespace Jiro.Core.DTO
+{
+    public class ChangePasswordDTO
+    {
+        public string CurrentPassword { get; set; } = default!;
+        public string NewPassword { get; set; } = default!;
+    }
+}
