2dc2fc513e590d45e61167fc9d0eb0a769906723
HueByte <ihuebyte@gmail.com>
2023-03-27 06:15:25 +0200

    welcome notification

--- src/Jiro.Kernel/Jiro.Api/clientapp/src/layouts/MainLayout.tsx
+++ src/Jiro.Kernel/Jiro.Api/clientapp/src/layouts/MainLayout.tsx
@@ -1,7 +1,15 @@
+import { useContext, useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+import { infoToast } from "../lib/notifications";
 import Menu from "./Menu";
 
 const MainLayout = () => {
+  const auth = useContext(AuthContext);
+  useEffect(() => {
+    infoToast(`Welcome ${auth?.authState?.username}!`);
+  }, []);
+
   return (
     <div className="z-50 flex h-screen w-full max-w-full flex-row overflow-y-scroll">
       <Menu />
