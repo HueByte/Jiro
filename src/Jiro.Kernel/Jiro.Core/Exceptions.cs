18bc48e35ba7ca40a87adb2c27aac89d59dc6dfb
HueByte <ihuebyte@gmail.com>
2023-06-24 20:08:00 +0200

    New Exception

--- src/Jiro.Kernel/Jiro.Core/Exceptions.cs
+++ src/Jiro.Kernel/Jiro.Core/Exceptions.cs
@@ -1,5 +1,26 @@
 namespace Jiro.Core;
 
+public class JiroException : Exception
+{
+    public string UserMessage { get; set; }
+    public string[] Details { get; set; }
+
+    public JiroException(string userMessage) : this(userMessage, Array.Empty<string>()) { }
+    public JiroException(string userMessage, params string[] details) : base(userMessage)
+    {
+        UserMessage = userMessage;
+        Details = details;
+    }
+
+    public JiroException(Exception exception, string userMessage) : this(exception, userMessage, Array.Empty<string>()) { }
+    public JiroException(Exception exception, string userMessage, params string[] details) : base(exception.Message, exception)
+    {
+        UserMessage = userMessage;
+        Details = details;
+    }
+}
+
+[Obsolete("Use JiroException instead")]
 public class HandledExceptionList : Exception
 {
     public ICollection<string> ExceptionMessages { get; set; }
@@ -9,11 +30,13 @@ public class HandledExceptionList : Exception
     }
 }
 
+[Obsolete("Use JiroException instead")]
 public class HandledException : Exception
 {
     public HandledException(string exceptionMessage) : base(exceptionMessage) { }
 }
 
+[Obsolete("Use JiroException instead")]
 public class TokenException : Exception
 {
     public TokenException(string exceptionMessage) : base(exceptionMessage) { }
