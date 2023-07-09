b53972d5ff9fb2b3abe93f9a78d08e8f16087116
HueByte <ihuebyte@gmail.com>
2023-07-09 05:23:35 +0200

    Working command exchange system

--- src/Jiro.Kernel/Jiro.App/JiroClientService.cs
+++ src/Jiro.Kernel/Jiro.App/JiroClientService.cs
@@ -1,9 +1,10 @@
 ﻿using Google.Protobuf;
 using Grpc.Core;
 using Grpc.Net.ClientFactory;
+using Jiro.Commands;
+using Jiro.Commands.Models;
 using Jiro.Core.Interfaces.IServices;
 using JiroCloud.Api.Proto;
-using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -17,11 +18,13 @@ internal class JiroClientService : IHostedService
     private readonly IServiceScopeFactory _scopeFactory;
     private readonly ILogger<JiroClientService> _logger;
     private readonly IServiceProvider _serviceProvider;
-    public JiroClientService(IServiceScopeFactory scopeFactory, ILogger<JiroClientService> logger, IServiceProvider serviceProvider)
+    private ICommandHandlerService _commandHandler;
+    public JiroClientService(IServiceScopeFactory scopeFactory, ILogger<JiroClientService> logger, IServiceProvider serviceProvider, ICommandHandlerService commandHandlerService)
     {
         _scopeFactory = scopeFactory;
         _logger = logger;
         _serviceProvider = serviceProvider;
+        _commandHandler = commandHandlerService;
     }
 
     /// <summary>
@@ -37,122 +40,43 @@ internal class JiroClientService : IHostedService
         {
             try
             {
-                await using var scope = _scopeFactory.CreateAsyncScope();
+                Dictionary<string, Task<ServerMessage>> commandQueue = new();
 
-                var commandHandler = scope.ServiceProvider.GetRequiredService<ICommandHandlerService>();
-                var factory = scope.ServiceProvider.GetRequiredService<GrpcClientFactory>();
+                await using var connectionScope = _scopeFactory.CreateAsyncScope();
+                var clientFactory = connectionScope.ServiceProvider.GetRequiredService<GrpcClientFactory>();
+                var currentClient = clientFactory.CreateClient<JiroHubProtoClient>("JiroClient");
 
-                var client = factory.CreateClient<JiroHubProtoClient>("JiroClient");
-
-                TaskCompletionSource<ServerMessage> serverListenerResponse = new(cancellationToken);
-
-                Grpc.Core.Metadata headers = new Grpc.Core.Metadata
+                Metadata connectionHeaders = new()
                 {
                     { "Content-Type", "application/grpc" }
                 };
 
-                // create connection
-                using var call = client.InstanceCommand(headers);
-
-                // create listening task
-                var listeningTask = Task.Run(async () =>
+                using var callInstance = currentClient.InstanceCommand(connectionHeaders, cancellationToken: cancellationToken);
+                var listeningLoop = Task.Run(async () =>
                 {
-                    try
+                    _logger.LogInformation("Connected to server");
+
+                    await foreach (var serverMessage in callInstance.ResponseStream.ReadAllAsync())
                     {
-                        _logger.LogInformation("Connected to server");
-
-                        // await for server messages
-                        await foreach (var response in call.ResponseStream.ReadAllAsync())
+                        if (commandQueue.TryGetValue(serverMessage.CommandSyncId, out _))
                         {
-                            try
-                            {
-                                _logger.LogInformation("Command arrived: [{command}]", response.Command);
-
-                                // notify response loop about new message
-                                serverListenerResponse.SetResult(response);
-                            }
-                            catch (Exception ex)
-                            {
-                                _logger.LogError(ex, ex.Message);
-                                serverListenerResponse.SetException(ex);
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex, ex.Message);
-                        serverListenerResponse.SetException(ex);
-                    }
-                });
-
-                // create response loop
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    try
-                    {
-                        _logger.LogInformation("Awaiting command");
-                        // await for server message
-                        var result = await serverListenerResponse.Task;
-                        serverListenerResponse = new();
-
-                        // create scope for command
-                        await using var commandScope = _scopeFactory.CreateAsyncScope();
-
-                        var currentUserService = commandScope.ServiceProvider.GetRequiredService<ICurrentUserService>();
-                        currentUserService.SetCurrentUser(result.InstanceName);
-
-                        // execute command from prompt
-                        var commandResult = await commandHandler.ExecuteCommandAsync(commandScope.ServiceProvider, result.Command);
-
-                        // todo
-                        // use mapper
-                        var commandType = GetCommandType(commandResult.CommandType);
-
-                        ClientMessage response = new()
-                        {
-                            CommandName = commandResult.CommandName,
-                            CommandType = commandType,
-                            IsSuccess = commandResult.IsSuccess
-                        };
-
-                        if (commandType == JiroCloud.Api.Proto.CommandType.Text)
-                        {
-                            response.TextResult = new()
-                            {
-                                Response = commandResult.Result?.Message
-                            };
-                        }
-                        else if (commandType == JiroCloud.Api.Proto.CommandType.Graph)
-                        {
-                            if (commandResult.Result is Jiro.Commands.Results.GraphResult graph)
-                            {
-                                response.GraphResult = new()
-                                {
-                                    Message = graph.Message,
-                                    Note = graph.Note,
-                                    XAxis = graph.XAxis,
-                                    YAxis = graph.YAxis,
-                                    GraphData = ByteString.CopyFrom(graph.Data as string, Encoding.UTF8)
-                                };
-
-                                response.GraphResult.Units.Add(graph.Units);
-                            }
+                            throw new Exception("Command already in queue");
                         }
 
-                        await call.RequestStream.WriteAsync(response);
+                        // capture variables
+                        var scopedCommandSyncId = serverMessage.CommandSyncId;
+                        var command = serverMessage.Command;
+                        var commandTask = Task.Run(async () =>
+                        {
+                            var commandResult = await ExecuteCommand(scopedCommandSyncId, command);
+                            
+                            await callInstance.RequestStream.WriteAsync(commandResult);
+                        });
                     }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex, ex.Message);
-                        serverListenerResponse = new();
-                    }
-                }
+                }, cancellationToken);
 
-                _logger.LogInformation("Disconnecting");
-
-                await call.RequestStream.CompleteAsync();
-                await listeningTask;
-
+                await listeningLoop;
+                await callInstance.RequestStream.CompleteAsync();
             }
             catch (RpcException ex)
             {
@@ -164,30 +88,78 @@ internal class JiroClientService : IHostedService
             }
             finally
             {
-
                 _logger.LogInformation("Attempting to reconnect in 5 seconds");
-                await Task.Delay(10_000);
+                await Task.Delay(10_000, cancellationToken);
             }
         } while (!cancellationToken.IsCancellationRequested);
     }
 
-    private async Task SayHello()
+    private async Task<ClientMessage> ExecuteCommand(string id, string command)
     {
+        await using var commandScope = _scopeFactory.CreateAsyncScope();
+        var commandResult = await _commandHandler.ExecuteCommandAsync(commandScope.ServiceProvider, command);
 
-        Grpc.Core.Metadata headers = new Grpc.Core.Metadata
+        return CreateMessage(id, commandResult);
+    }
+
+    private ClientMessage CreateMessage(string syncId, CommandResponse commandResult)
+    {
+        // todo
+        // use mapper later
+        var commandType = GetCommandType(commandResult.CommandType);
+
+        ClientMessage response = new()
         {
-            { "Content-Type", "application/grpc" }
+            CommandSyncId = syncId, 
+            CommandName = commandResult.CommandName,
+            CommandType = commandType,
+            IsSuccess = commandResult.IsSuccess
         };
 
-        await using var scope = _scopeFactory.CreateAsyncScope();
-        var factory = scope.ServiceProvider.GetRequiredService<GrpcClientFactory>();
+        if (commandType == JiroCloud.Api.Proto.CommandType.Text)
+        {
+            response.TextResult = new()
+            {
+                Response = commandResult.Result?.Message
+            };
+        }
+        else if (commandType == JiroCloud.Api.Proto.CommandType.Graph)
+        {
+            if (commandResult.Result is Jiro.Commands.Results.GraphResult graph)
+            {
+                response.GraphResult = new()
+                {
+                    Message = graph.Message,
+                    Note = graph.Note,
+                    XAxis = graph.XAxis,
+                    YAxis = graph.YAxis,
+                    GraphData = ByteString.CopyFrom(graph.Data as string, Encoding.UTF8)
+                };
 
-        var client = factory.CreateClient<JiroHubProtoClient>("JiroClient");
+                response.GraphResult.Units.Add(graph.Units);
+            }
+        }
 
-        var helloCall = await client.HelloAsync(new HelloRequest() { Message = "Hello From Client" }, headers);
-
-        _logger.LogInformation(helloCall.Message);
+        return response;
     }
+
+    // private async Task SayHello()
+    // {
+
+    //     Grpc.Core.Metadata headers = new Grpc.Core.Metadata
+    //     {
+    //         { "Content-Type", "application/grpc" }
+    //     };
+
+    //     await using var scope = _scopeFactory.CreateAsyncScope();
+    //     var factory = scope.ServiceProvider.GetRequiredService<GrpcClientFactory>();
+
+    //     var client = factory.CreateClient<JiroHubProtoClient>("JiroClient");
+
+    //     var helloCall = await client.HelloAsync(new HelloRequest() { Message = "Hello From Client" }, headers);
+
+    //     _logger.LogInformation(helloCall.Message);
+    // }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
