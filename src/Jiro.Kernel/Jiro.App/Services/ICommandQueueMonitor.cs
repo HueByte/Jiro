113ed9e1a4d24d96cf267c205abc26f987f86877
HueByte <ihuebyte@gmail.com>
2025-07-12 01:40:24 +0200

    formatting

--- src/Jiro.Kernel/Jiro.App/Services/ICommandQueueMonitor.cs
+++ src/Jiro.Kernel/Jiro.App/Services/ICommandQueueMonitor.cs
@@ -5,28 +5,28 @@ namespace Jiro.App.Services;
 /// </summary>
 public interface ICommandQueueMonitor
 {
-    /// <summary>
-    /// Gets the current number of executing commands
-    /// </summary>
-    int ActiveCommandCount { get; }
+	/// <summary>
+	/// Gets the current number of executing commands
+	/// </summary>
+	int ActiveCommandCount { get; }
 
-    /// <summary>
-    /// Gets the list of currently executing command IDs
-    /// </summary>
-    IEnumerable<string> ActiveCommandIds { get; }
+	/// <summary>
+	/// Gets the list of currently executing command IDs
+	/// </summary>
+	IEnumerable<string> ActiveCommandIds { get; }
 
-    /// <summary>
-    /// Gets the total number of commands processed since startup
-    /// </summary>
-    long TotalCommandsProcessed { get; }
+	/// <summary>
+	/// Gets the total number of commands processed since startup
+	/// </summary>
+	long TotalCommandsProcessed { get; }
 
-    /// <summary>
-    /// Gets the number of commands that completed successfully
-    /// </summary>
-    long SuccessfulCommands { get; }
+	/// <summary>
+	/// Gets the number of commands that completed successfully
+	/// </summary>
+	long SuccessfulCommands { get; }
 
-    /// <summary>
-    /// Gets the number of commands that failed
-    /// </summary>
-    long FailedCommands { get; }
+	/// <summary>
+	/// Gets the number of commands that failed
+	/// </summary>
+	long FailedCommands { get; }
 }
